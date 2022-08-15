2f226f066253bcb350a96dadd0136a606854301b
zelrdev <jacob@zelr.me>
2022-08-15 16:48:46 +0100

    fix rediects

--- app/routes/dashboard/teams/index.tsx
+++ app/routes/dashboard/teams/index.tsx
@@ -1,12 +1,16 @@
-import { Link, useLocation, useNavigate } from "@remix-run/react";
+import { useNavigate } from "@remix-run/react";
 import { useEffect } from "react";
 import type { getTeams } from "~/models/team.server";
 import { useMatchesData } from "~/utils";
 
 export default function IndexPage() {
   const navigate = useNavigate();
+  const data = useMatchesData("routes/dashboard") as {
+    teams: Awaited<ReturnType<typeof getTeams>>;
+  };
   useEffect(() => {
-    navigate("/dashboard/teams/view");
+    if (data.teams.length <= 0) navigate("/dashboard/teams/new");
+    else navigate("/dashboard/teams/view");
   });
   return <div></div>;
 }
